3c7d662f355b82804da14fa5593eb3d98d29da5c
Pooja Bist <poojakumari-lokbahadur.bist@capgemini.com>
2021-10-03 17:04:54 +0530

    Feat : to display 3 robos

--- async/index.js
+++ async/index.js
@@ -1,6 +1,6 @@
 var fs  = require ('fs');
 const superagent = require('superagent');
-var myurl = require('url');
+let myurl = require('url');
 
 var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
@@ -11,11 +11,6 @@ function randomText(length) {
    }
    return result;
 }
-
-
-
-
-
 
 function writeFilePromise(fileLocation, data){
     return new Promise((resolve , reject)=>{
@@ -31,19 +26,41 @@ function writeFilePromise(fileLocation, data){
 
 async function getRoboPic(){
     try{
-    var data = randomText(10);;
-    console.log(`The string generated is ${data}`);
-    const res = await superagent.get(
-        `https://robohash.org/${data}`
-    
+    let data1 = randomText(10);
+    console.log(`The string generated is ${data1}`);
+    const res1 = await superagent.get(
+        `https://robohash.org/${data1}`
     );
-    myurl = `https://robohash.org/${data}`
+    let data2 = randomText(5);;
+    console.log(`The string generated is ${data2}`);
+    const res2 = await superagent.get(
+        `https://robohash.org/${data2}`
+    );
+
+    let data3 = randomText(7);;
+    console.log(`The string generated is ${data3}`);
+    const res3 = await superagent.get(
+        `https://robohash.org/${data3}`
+    );
+
+    
+    const all = await Promise.all([res1 , res2 ,res3]) 
+    console.log[all];
+    /*myurl = `https://robohash.org/${data}`
     console.log(myurl);
     writeFilePromise("./robotImage", myurl);
-    console.log("Successfuuly written inside the file");
+    console.log("Successfuuly written inside the file");*/
+    
+    myurl = [`https://robohash.org/${data1}`,`https://robohash.org/${data2}`,`https://robohash.org/${data3}`]
 
+    for ( let i = 0 ; i< 3 ; i++){
+   // myurl = `https://robohash.org/${data[i]}`
+    console.log(myurl[i]);
+    writeFilePromise("./robotImage.txt", myurl);
+    console.log("Successfully written inside the file");
+    }  
  }catch(err){
-    console.log(err)
+    //console.log(err)
 }
 }
 
